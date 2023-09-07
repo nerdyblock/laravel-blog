ddd5fca7c2bde6443f66899a94e405eac693a066
Akash Pal <creativeakashpal@gmail.com>
2023-09-07 18:37:04 +0530

    ep-32: blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
-    @foreach ($posts as $post)
+    @include('_post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else 
+            <h1 class="text-center">No posts yet. Please Come Back Later!</h1>
+        @endif
+    </main>
+    <!-- @foreach ($posts as $post)
     <article>
         <h1><a href="/posts/{{ $post->slug }}">{{$post->title}}</a></h1>
 
@@ -20,5 +29,5 @@
             </p>
         </div>
     </article>
-    @endforeach
+    @endforeach -->
 </x-layout>
