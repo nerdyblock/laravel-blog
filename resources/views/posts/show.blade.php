1d5f87cb57b29de8ed39d77827e6474d78b7ff36
Akash Pal <creativeakashpal@gmail.com>
2023-09-21 16:25:30 +0530

    ep-54: comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -48,6 +48,13 @@
                     <p>{!! $post->body !!}</p>
                 </div>
             </div>
+
+            <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                @foreach ($post->comments as $comment)
+                    <x-post-comment :comment="$comment"/>
+                @endforeach
+                
+            </section>
         </article>
     </main>
 
